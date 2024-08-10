83c4e0193a86af42953cb4716c9467d303d69c58
Pegasis <me@pegasis.site>
2024-08-09 20:39:09 -0400

    fix clock

--- src/clock.rs
+++ src/clock.rs
@@ -28,10 +28,11 @@ impl DelayNs for Delay {
 
 impl DelayTrait for Delay {
     async fn delay_ms(&self, mut ms: f64) {
-        while ms > 4294f64 {
-            ms -= 4294f64;
-            EmbassyDelay.delay_ns(4_294_000_000).await;
-        }
-        EmbassyDelay.delay_ns((ms * 1000_1000.0) as u32).await
+        EmbassyDelay.delay_ms(ms as u32).await;
+        // while ms > 4294f64 {
+        //     ms -= 4294f64;
+        //     EmbassyDelay.delay_ns(4_294_000_000).await;
+        // }
+        // EmbassyDelay.delay_ns((ms * 1000_1000.0) as u32).await
     }
 }
