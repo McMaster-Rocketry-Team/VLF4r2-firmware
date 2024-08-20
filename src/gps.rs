f4b304823d76de0a62a5c5eabb6e9858c2484622
pegasis <me@pegasis.site>
2024-08-20 15:35:53 -0400

    fix gps

--- src/gps.rs
+++ src/gps.rs
@@ -1,15 +1,15 @@
 use core::cell::RefCell;
 
+use cortex_m::singleton;
 use defmt::{trace, warn};
 use embassy_stm32::{
     bind_interrupts,
     exti::ExtiInput,
     gpio::{Level, Output, Speed},
-    mode::Async,
     peripherals::{DMA2_CH0, DMA2_CH1, PA2, PA3, PE9, USART2},
-    usart::{self, Config as UartConfig, Error, RingBufferedUartRx, Uart},
+    usart::{self, BufferedUart, Config as UartConfig, Error, RingBufferedUartRx, Uart},
 };
-use embedded_io_async::Read;
+use embedded_io_async::{Read, Write};
 use firmware_common::driver::uart_gps::UARTGPS as CommonUartGPS;
 use firmware_common::{
     common::sensor_reading::SensorReading,
@@ -19,11 +19,11 @@ use firmware_common::{
 use crate::sleep;
 
 bind_interrupts!(struct Irqs {
-    USART2 => usart::InterruptHandler<USART2>;
+    USART2 => usart::BufferedInterruptHandler<USART2>;
 });
 
 pub struct UartGPS {
-    uart: RefCell<Option<Uart<'static, Async>>>,
+    uart: RefCell<BufferedUart<'static>>,
     gps: CommonUartGPS,
     nrst: Output<'static>,
 }
@@ -31,90 +31,53 @@ pub struct UartGPS {
 impl UartGPS {
     pub fn new(
         nsrt: PE9,
-        _uart: USART2,
-        _rx: PA3,
-        _tx: PA2,
+        uart: USART2,
+        rx: PA3,
+        tx: PA2,
         _rx_dma: DMA2_CH0,
         _tx_dma: DMA2_CH1,
     ) -> Self {
+        let tx_buf = singleton!(: [u8; 32] = [0; 32]).unwrap();
+        let rx_buf = singleton!(: [u8; 32] = [0; 32]).unwrap();
+
+        let mut config = UartConfig::default();
+        config.baudrate = 9600;
+
         Self {
-            uart: RefCell::new(None),
+            uart: RefCell::new(
+                BufferedUart::new(uart, Irqs, rx, tx, tx_buf, rx_buf, config).unwrap(),
+            ),
             gps: CommonUartGPS::new(),
             nrst: Output::new(nsrt, Level::High, Speed::Low),
         }
     }
 
-    fn create_uart(baudrate: u32) -> Uart<'static, Async> {
-        let mut config = UartConfig::default();
-        config.baudrate = baudrate;
-
-        // Safety: peripherals stealed here are all claimed by the constructor.
-        unsafe {
-            Uart::new(
-                USART2::steal(),
-                PA3::steal(),
-                PA2::steal(),
-                Irqs,
-                DMA2_CH1::steal(),
-                DMA2_CH0::steal(),
-                config,
-            )
-            .unwrap()
-        }
-    }
-
     pub async fn reset(&mut self) {
-        if let Some(uart) = self.uart.borrow_mut().take() {
-            drop(uart);
-        }
-
         self.nrst.set_low();
         sleep!(10);
         self.nrst.set_high();
         sleep!(1000);
 
-        let mut uart = Self::create_uart(9600);
+        let mut uart = self.uart.borrow_mut();
+        let mut config = UartConfig::default();
+        config.baudrate = 9600;
+        uart.set_config(&config).unwrap();
+
         uart.write("$PMTK251,38400*27\r\n".as_bytes())
             .await
             .unwrap();
         sleep!(1000);
-        drop(uart);
 
-        let mut uart = Self::create_uart(38400);
+        config.baudrate = 38400;
+        uart.set_config(&config).unwrap();
+
         uart.write("$PMTK255,1*2D\r\n".as_bytes()).await.unwrap();
         sleep!(200);
-
-        self.uart.replace(Some(uart));
     }
 
     pub async fn run(&self, clock: impl Clock) -> Result<(), Error> {
-        let uart = self.uart.borrow_mut().take().unwrap();
-        let rx = uart.split().1;
-        let mut buffer = [0u8; 84];
-        let rx = rx.into_ring_buffered(&mut buffer);
-        let mut rx = RingBufferedRxOverrunWrapper(rx);
-        self.gps.run(&mut rx, clock).await;
-    }
-}
-
-struct RingBufferedRxOverrunWrapper<'a>(RingBufferedUartRx<'a>);
-
-impl<'a> embedded_io_async::ErrorType for RingBufferedRxOverrunWrapper<'a> {
-    type Error = Error;
-}
-
-impl<'a> embedded_io_async::Read for RingBufferedRxOverrunWrapper<'a> {
-    async fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
-        let result = self.0.read(buf).await;
-        match result {
-            Ok(size) => Ok(size),
-            Err(Error::Overrun) => {
-                warn!("Uart overrun");
-                self.0.start()?;
-                Err(Error::Overrun)
-            }
-            Err(e) => Err(e),
-        }
+        let mut uart = self.uart.borrow_mut();
+        self.gps.run(&mut *uart, clock).await;
     }
 }
 
